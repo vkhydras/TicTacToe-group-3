f081162cf7a80d5da7ef4ee009507454650030e0
Caldavis Irurah <caldaviswangaruro@gmail.com>
2023-06-09 09:47:25 +0300

    Added the changePlayer function
    This function shows which player is supposed to play at a given round.

--- index.js
+++ index.js
@@ -6,7 +6,37 @@ let roundWon = false
 let restartBtn= document.getElementById('restart')
 restartBtn.addEventListener('click',restart)
 let draw = true
+function clickHandle(e){ 
+   if (gameStatus == true && roundWon == false){
+      changePlayer(e)
+      checkWinner()
+   } 
+   
+}
 
+function changePlayer(e){
+   if (turn==player_x){
+      e.target.append(turn)
+      turn = player_o
+      document.getElementById('X').style.backgroundColor = 'white'
+      document.getElementById('X').style.color = 'black'
+      document.getElementById('O').style.backgroundColor = 'black'
+      document.getElementById('O').style.color = 'white'
+   } else{
+      e.target.append(turn)
+      turn = player_x
+      document.getElementById('O').style.backgroundColor = 'white'
+      document.getElementById('O').style.color = 'black'
+      document.getElementById('X').style.backgroundColor = 'black'
+      document.getElementById('X').style.color = 'white'
+      
+   }
+   document.getElementById("turn").innerHTML =turn + "'S " + " TURN"
+   const id = e.target.id
+   if (!options[id]){
+      options[id] = turn
+   }
+}
 
 
 let gameStatus = true;
